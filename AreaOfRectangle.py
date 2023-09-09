04a4146fcb1ef8b57de96e2b03c81a4253c28888
POGULA SAI SRI VARSHA <115077878+Varshapogula@users.noreply.github.com>
2023-09-09 11:47:26 +0530

    Create AreaOfRectangle.py

---
+++ AreaOfRectangle.py
@@ -0,0 +1,4 @@
+length = float(input("Enter the length of a rectangle : "))
+breadth = float(input("Enter the breadth of a rectangle : "))
+Area = length * breadth
+print("Area of a Rectangle is: %2f" %Area)
